e3c31258d660a9c652b25495778bbc5b2bc9a04e
DerickSiecola <128324921+DerickSiecola@users.noreply.github.com>
2025-11-25 12:43:00 -0300

    Update urls.py
    Agora o usuário pode acessar a versão pública do perfil de outro usuário.

--- backend/Saboreie/autenticacao/urls.py
+++ backend/Saboreie/autenticacao/urls.py
@@ -5,8 +5,11 @@ urlpatterns = [
     path('login/', views.paginaLogin, name="paginaLogin"),
     path('logout/', views.logoutUser, name="logout"),
     path('registrar/', views.registrarUser, name="registrarUser"),
-    path('perfil/', views.perfil, name="perfil"),
-    path('perfil/<str:username>/', views.perfil, name="perfil_usuario"),
+
+    path('perfil/', views.perfil, name="perfil"),  # Perfil do usuário logado
+    path('perfil/<int:user_id>/', views.perfil_publico, name="perfil_publico"),
+
     path('editar-perfil/', views.editar_perfil, name="editar_perfil"),
+
     path('', views.home, name="home"),
 ]
